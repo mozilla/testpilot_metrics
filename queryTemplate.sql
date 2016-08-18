e9228268270112d245e6dc63a414925496a80a1a
Rob <rrayborn@mozilla.com>
2016-08-18 14:57:06 -0700

    Add URL field

--- queryTemplate.sql
+++ queryTemplate.sql
@@ -2,10 +2,10 @@
  * Owner: <username>@mozilla.com
  * Reviewer: <username>@mozilla.com
  * Status: <In-Progress|Draft|Reviewed>
+ * URL: <redash url>
  * Dashboard(s):
-    * <url>
-    * <url>
+    * <redash url>
+    * <redash url>
  */
 
- 
  /* SQL goes here.  Remember to add comments to make it easy to parse. */
